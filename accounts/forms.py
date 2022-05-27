25e83396bf57b2c7919953184322a368615d2e88
KnQ4 <Kcodewn@gmail.com>
2022-05-27 18:47:18 -0500

    Added login and register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,14 @@
-from ast import Mod
-from pyexpat import model
-from re import L
-from statistics import mode
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import *
 from .models import *
-
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
